3164359722e147cb94dd8d3bcfbfec87692d9b33
Rodasac <adolfo.bastardo@gmail.com>
2014-06-28 16:11:28 -0430

    Removiendo archivos no usados

--- util/modulos/Auth/Auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- *
- */
-// Base de datos
-$dsn = 'mysql:dbname=u897309850_skate;host=mysql.nixiweb.com';
-$u = 'u897309850_skate';
-$p = 'republic12&22';
-Sentry::setupDatabaseResolver( new PDO($dsn, $u, $p));
